95a0d7954a418030f2eb392dbb0a2ffe9c1378b0
sajal596 <sajal596@diu.edu.bd>
2020-03-20 04:09:09 +0600

    this is first commit.

---
+++ onlinebookstore2/src/main/java/com/softwarebd/org/Onlinebookstore2Application.java
@@ -0,0 +1,13 @@
+package com.softwarebd.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onlinebookstore2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onlinebookstore2Application.class, args);
+	}
+
+}
